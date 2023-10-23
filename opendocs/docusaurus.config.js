b9c7168838f415c0ae946a528614d4d960388e90
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-10-23 19:04:30 +0100

    Configure docusaurus

--- opendocs/docusaurus.config.js
+++ opendocs/docusaurus.config.js
@@ -4,22 +4,41 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+require('dotenv').config();
+
+
+const getBaseUrl = () => {
+  if (process.env.NETLIFY) {
+      // Netlify hosts on '/', always.
+      return '/';
+  }
+
+  if (typeof process.env.BASEURL !== 'undefined') {
+      // Respect the env.
+      return process.env.BASEURL;
+  }
+
+  // Default is currently '/opendocs'.
+  return '/opendocs/';
+};
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Rootwire',
+  tagline: 'Rootwire Resources',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: process.env?.URL || 'https://opendocs.rootwire.co.uk',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  baseUrl: getBaseUrl(),
+  trailingSlash: false,
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rootwire', // Usually your GitHub org/user name.
+  projectName: 'opendocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -28,8 +47,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'en-GB',
+    locales: ['en-GB'],
   },
 
   presets: [
@@ -42,15 +61,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/rootwire/opendocs/edit/main/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
